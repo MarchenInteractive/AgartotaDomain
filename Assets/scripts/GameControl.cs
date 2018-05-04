9fd64f8852bd52dd11e52893aba719daf44ad2a1
vuraul94 <vuraul94@gmail.com>
2018-05-03 22:15:08 -0600

    first elements commit :heavy_plus_sign:

---
+++ Assets/scripts/GameControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+
+    public static GameControl instance;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        if (GameControl.instance == null)
+        {
+            GameControl.instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    public void LoadScene(string value)
+    {
+        SceneManager.LoadScene(value);
+    }
+}
---
+++ Assets/scripts/Utilities.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Utilities : MonoBehaviour
+{
+    public string sceneTarget = "Intro";
+    public bool timer = false;
+    public float waitTime = 3f;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (timer)
+        {
+            StartCoroutine(ScenesSwitcher());
+        }
+    }
+
+    public void LoadSceneUI(string value)
+    {
+        GameControl.instance.LoadScene(value);
+    }
+
+    private IEnumerator ScenesSwitcher()
+    {
+        yield return new WaitForSeconds(waitTime);
+        Debug.Log("Cambio de escena");
+        SceneManager.LoadScene(sceneTarget);
+    }
+}
